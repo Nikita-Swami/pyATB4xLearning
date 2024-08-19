31cb9df7d84a8762308e569212073b8bc9d2355b
nikita.swami <nikitaswami09@gmail.com>
2024-08-19 08:29:29 +0530

    All projects

---
+++ src/Tasks/Task2.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter number1"))
+num2 = float(input("Enter number2"))
+
+sum = print("sum is", num1+num2)
+sub = print("sub is", num1-num2)
+mul = print("mul is", num1*num2)
+div = print("div is", num1/num2)
+max = print("max is", max(num1,num2))
+pow = print("pow is", pow(num1,num2))
