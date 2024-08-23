c78c5057c54474f95f43d32e3d96f32bbc2c17de
nikita.swami <nikitaswami09@gmail.com>
2024-08-23 22:40:12 +0530

    All projects

--- src/Tasks/Task11.py
+++ src/Tasks/Task11.py
@@ -1,2 +1,10 @@
 #Task #11 -
 #✅ Fibonaci series 0,0+1, 0+1+1, n = 7 0, 1, 2, 3, 5, 8, 13
+
+num= int(input("Enter the number"))
+fib=[0,1]+[0]*(num-1)
+for i in range(2,num+1):
+    fib[i]=fib[i-1]+fib[i-2]
+    print("Fibonacci is:", fib)
+
+
