57b3e2416ca2fa4fc28c6e7b4e97e4a7fccaea6c
nikita.swami <nikitaswami09@gmail.com>
2024-08-29 21:31:11 +0530

    All projects

---
+++ src/ex_22082024/Lab011.py
@@ -0,0 +1,8 @@
+#Continue
+
+for number in range(10):
+    if number %2 == 0:
+        continue
+    else:
+        print(number)
+
---
+++ src/ex_22082024/Lab012.py
@@ -0,0 +1,18 @@
+# Match Statement
+# Same like Switch in Java
+
+# Write a program to ask user which browser you can run for automation
+
+browser_name = input("Enter the name of browser/n")
+browser_name = browser_name.lower()
+match browser_name:
+    case "firefox":
+        print("Execute the firefox code")
+    case "chrome":
+        print("Execute the chrome code")
+    case "edge":
+        print("Execute the edge code")
+    case "safari":
+        print("Execute the safari code")
+    case _:
+        print("Browser not found!")
